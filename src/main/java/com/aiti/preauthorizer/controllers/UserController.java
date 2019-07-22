ea1f44accd6df7bcd1491c6800a76a4a7c099099
GabrielMorenoGarrido <gabomgar@gmail.com>
2019-07-22 15:43:54 -0500

    Cambiando contest path de la api

--- src/main/java/com/aiti/preauthorizer/controllers/UserController.java
+++ src/main/java/com/aiti/preauthorizer/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.aiti.preauthorizer.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@RestController
+@Controller
 @RequestMapping("/me")
 public class UserController {
 
